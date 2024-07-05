febe1d4107e80a5ab1966fb8bc340a8b67f9a1e9
yxsy <56529455+yx-sy@users.noreply.github.com>
2024-07-05 22:04:28 +0800

    Add files via upload

--- app/src/main/java/com/github/catvod/utils/Image.java
+++ app/src/main/java/com/github/catvod/utils/Image.java
@@ -2,8 +2,9 @@ package com.github.catvod.utils;
 
 public class Image {
 
-    public static final String FOLDER = "https://s1.ax1x.com/2023/05/04/p9tgI81.png";
-    public static final String VIDEO = "https://s1.ax1x.com/2023/05/04/p9tgogx.png";
+    public static final String FOLDER = "https://gitee.com/yx-sy/img/raw/main/cus/folder.png";
+    public static final String VIDEO = "https://gitee.com/yx-sy/img/raw/main/cus/video.png";
+    public static final String PUSH = "https://gitee.com/yx-sy/img/raw/main/cus/push.png";
 
     public static String getIcon(boolean folder) {
         return folder ? FOLDER : VIDEO;
